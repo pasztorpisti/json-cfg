3a7191e793e5d0ddba94e3333980bfa99102a91a
István Pásztor <pasztorpisti@gmail.com>
2015-05-01 21:57:03 +0100

    changed the documentation for the versioning rules

--- jsoncfg/__init__.py
+++ jsoncfg/__init__.py
@@ -24,9 +24,15 @@ __all__ = [
     'JSONValueConverter',
 ]
 
-# version_info[0]: increase in case of large rewrites that are not backward compatible
-# version_info[1]: increase in case of adding new features that keep old ones backward compatible
-# version_info[2]: adding only bugfixes without interface modification
+# version_info[0]: Increase in case of large milestones/releases.
+# version_info[1]: Increase this and zero out version_info[2] if you have explicitly modified
+#                  a previously existing behavior/interface.
+#                  If the behavior of an existing feature changes as a result of a bugfix
+#                  and the new (bugfixed) behavior is that meets the expectations of the
+#                  previous interface documentation then you shouldn't increase this, in that
+#                  case increase only version_info[2].
+# version_info[2]: Increase in case of bugfixes. Also use this if you added new features
+#                  without modifying the behavior of the previously existing ones.
 version_info = (0, 2, 0)
 __version__ = '.'.join(str(n) for n in version_info)
 __author__ = 'István Pásztor'
